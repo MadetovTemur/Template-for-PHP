878f353e4d40c5353f578b867039dd2d6802ce7d
Madetov_017 <temur1735@gmail.com>
2023-10-03 21:29:10 +0500

    1.0x

---
+++ index.php
@@ -0,0 +1,11 @@
+<?
+ $document_title = 'index';
+ $document_lang = 'uz';
+ require __DIR__ . '/layout/body.php'; 
+?>
+
+<h1>dwedewd</h1>
+
+<?
+require __DIR__ . '/layout/footer.php';
+?>
---
+++ layout/body.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="<?php echo $document_lang ?? 'en' ?>">
+<head>
+  <meta charset="<?php echo $document_charset ?? 'UTF-8' ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $document_title ?? 'Requiret title' ?></title>
+</head>
+<body>
+  <header>
+    <h1>Main hader</h1>
+  </header>
+  <main>
